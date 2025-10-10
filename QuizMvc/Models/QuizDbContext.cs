1676d5801071eb884a208d1aff0096a09a48438c
saabd6875 <saabd6875@oslomet.no>
2025-10-10 16:29:43 +0200

    Answer

--- QuizMvc/Models/QuizDbContext.cs
+++ QuizMvc/Models/QuizDbContext.cs
@@ -7,7 +7,7 @@ public class QuizDbContext : DbContext
     public DbSet<Category> Categories { get; set; }
     public DbSet<Quiz> Quizzes { get; set; }
     public DbSet<Question> Questions { get; set; }
-    public DbSet<Answers> Answers { get; set; }
+    public DbSet<Answers> Answer { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlite("Data Source=quiz.db");
