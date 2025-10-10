53e972e71d9e08ff950a2dc07fe5820130c4f6eb
wakre <wakre6980@oslomet.no>
2025-10-10 15:15:19 +0200

    answer

--- QuizMvc/Models/answers.cs
+++ QuizMvc/Models/answers.cs
@@ -1,13 +1,15 @@
 using System;
 
-namespace QuizMvc.Models {
+namespace QuizMvc.Models
+{
 
-public class Answers {
-    public int AnswerID { get; set; }
-    public string Tekst { get; set; }
-    public bool IsCorrect { get; set; }
+    public class Answers
+    {
+        public int AnswerId { get; set; }
+        public string Tekst { get; set; }
+        public bool IsCorrect { get; set; }
 
-    public int QuestionID { get; set; }
-    public Question Question {get; set; }
-}
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+    }
 }
--- QuizMvc/obj/Debug/net8.0/QuizMvc.AssemblyInfo.cs
+++ QuizMvc/obj/Debug/net8.0/QuizMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1289fc8921276d5dc817f3c179b3d8fe6de5deb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03cf024e477b5c76cb4f3fcf99d9f20c33027c77")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
