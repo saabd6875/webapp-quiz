ecfa0e7da23b5843dbeb37bd534994fb62189609
Gjanany <gjananyk@gmail.com>
2025-10-10 16:26:31 +0200

    endret question

--- QuizMvc/Models/Questions.cs
+++ QuizMvc/Models/Questions.cs
@@ -1,15 +1,40 @@
-using System;
+
+using System.ComponentModel.DataAnnotations;
 
 namespace QuizMvc.Models
 {
-    public class Question
+    public class Question : IValidatableObject
     {
-        public int QuestionId { get; set; } // Unik ID for spørsmålet
-        public string Text { get; set; } = string.Empty; // Selve spørsmålet
-        public List<string> Options { get; set; } = new List<string>(); // Liste over svaralternativer
-        public string CorrectAnswer { get; set; } = string.Empty; // Riktig svar på spørsmålet
-        public int QuizId { get; set; } // ID til quizen som dette spørsmålet hører til
-    
-    }
+        public int QuestionId { get; set; }
 
+        [Required(ErrorMessage = "Please add a question")]
+        public string Text { get; set; } = string.Empty;
+
+        public List<Answers> Answers { get; set; } = new List<Answers>();
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Answers == null || Answers.Count == 0)
+            {
+                yield return new ValidationResult("The question must have at least one correct answer",
+                new[] { nameof(Answers) });
+            }
+            else
+            {
+                if (Answers.Count > 4)
+                {
+                    yield return new ValidationResult("The question has a maximum of 4 correct answers",
+                    new[] { nameof(Answers) });
+                }
+
+                if (!Answers.Any(a => a.IsCorrect))
+                {
+                    yield return new ValidationResult("Minst ett alternativ må være riktig",
+                    new[] { nameof(Answers) });
+                }
+            }
+        }
+
+        public int QuizId { get; set; }
+    }
 }
