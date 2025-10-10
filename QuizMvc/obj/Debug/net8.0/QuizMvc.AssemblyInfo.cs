e00c16000c4fd6a1dc8595bbcecacaf9b0b3e947
saabd6875 <saabd6875@oslomet.no>
2025-10-10 13:34:47 +0200

    små endringer

--- QuizMvc/obj/Debug/net8.0/QuizMvc.AssemblyInfo.cs
+++ QuizMvc/obj/Debug/net8.0/QuizMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+635c00cf9fa84d5a741b5b4e63ccb14120729d43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4e666e9233cc4bea28ac1a8d502cf95bb7c742")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
