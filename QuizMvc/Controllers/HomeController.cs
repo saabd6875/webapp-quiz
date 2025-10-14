22ee55f56746516f00a77f28c0257d96f17de47f
wakre <wakre6980@oslomet.no>
2025-10-14 14:32:47 +0200

    startet with index.cshtml

--- QuizMvc/Controllers/HomeController.cs
+++ QuizMvc/Controllers/HomeController.cs
@@ -3,9 +3,18 @@ using Microsoft.AspNetCore.Mvc;
 namespace QuizMvc.Controllers
 {
     public class Homecontroller : Controller {
-    public IActionResult Index()
-    {
-        return View();
+        public IActionResult Index()
+        {
+            return View();
         }
+        public IActionResult MakeQuiz()
+        {
+            return RedirectToAction("CreateQuize", "Quize");
+        }
+        public IActionResult TakeQuiz()
+        {
+            return RedirectToAction("TakeQuiz", "Quiz");
+        }
+        
     }
 }
