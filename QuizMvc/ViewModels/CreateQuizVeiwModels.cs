00795468f58d1f79a236a1d1b88457879a27213d
wakre <wakre6980@oslomet.no>
2025-10-16 20:06:56 +0200

    made ViewModel folder and two file one for creating quiz and one for taking quiz

---
+++ QuizMvc/ViewModels/CreateQuizVeiwModels.cs
@@ -0,0 +1,22 @@
+using QuizMvc.Models;
+
+namespace QuizMvc.ViewModels
+{
+   public class CreateQuizViewModels
+    {
+        public string Title { get; set; } = string.Empty; 
+        public string? Description { get; set; }
+        public int CategoryId { get; set; }
+        public List<QuestionInput> Questions { get; set; } = new();
+
+        public class QuestionInput
+        {
+            public string Text { get; set; } = string.Empty;
+            public string OptionA { get; set; } = string.Empty;
+            public string OptionB { get; set; } = string.Empty;
+            public string OptionC { get; set; } = string.Empty;
+            public string OptionD { get; set; } = string.Empty;
+            public string CorrectOption { get; set; } = string.Empty; 
+        }
+    }
+}
---
+++ QuizMvc/ViewModels/TakeQuizViewModels.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace QuizMvc.ViewModels
+{
+  public class TakeQuizViewModels
+    {
+        public int QuizId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public List<QuestionItem> Question { get; set; } = new();
+        public List<string> UserAnswes { get; set; } = new();
+
+        public class QuestionItem
+        {
+            public int Id { get; set; }
+            public string Text { get; set; } = string.Empty;
+            public string OptionA { get; set; } = string.Empty;
+            public string OptionB { get; set; } = string.Empty;
+            public string OptionC { get; set; } = string.Empty;
+            public string OptionD { get; set; } = string.Empty; 
+            
+        }
+    
+    }
+}
